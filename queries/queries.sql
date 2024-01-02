38df5f4dc029cb1c4847929f878636009031c5f8
Eren Yasa <erenyasa80@gmail.com>
2024-01-02 20:55:46 +0300

    Query for slip scanning is implemented

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,10 +1,10 @@
----Query for returning all the prices for a product in ascending order
+--- Query for returning all the prices for a product in ascending order
 
 SELECT p.name, pr.price
 FROM products p
 JOIN prices pr ON p.gtin = pr.product_id
 WHERE p.gtin = 8690504063476
-ORDER BY pr.price ASC
+ORDER BY pr.price ASC;
 
 --- Query for returning the lowest price product
 SELECT p.name, pr.price
@@ -12,4 +12,10 @@ FROM products p
 JOIN prices pr ON p.gtin = pr.product_id
 WHERE p.gtin = 8690504063476
 ORDER BY pr.price ASC
-LIMIT 1
+LIMIT 1;
+
+--- Query for inserting a new price from a scanned slip
+INSERT INTO prices (price, product_id)
+SELECT NEW_PRICE, p.gtin
+FROM products p
+WHERE p.name = PRODUCT_NAME_FROM_THE_SLIP_SCAN;

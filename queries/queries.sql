23777d432f0151b45ab73abce5febd7378a404f1
Eren Yasa <erenyasa80@gmail.com>
2024-01-02 20:44:49 +0300

    Query which returns the lowest priced product is written

--- queries/queries.sql
+++ queries/queries.sql
@@ -5,3 +5,11 @@ FROM products p
 JOIN prices pr ON p.gtin = pr.product_id
 WHERE p.gtin = 8690504063476
 ORDER BY pr.price ASC
+
+--- Query for returning the lowest price product
+SELECT p.name, pr.price
+FROM products p
+JOIN prices pr ON p.gtin = pr.product_id
+WHERE p.gtin = 8690504063476
+ORDER BY pr.price ASC
+LIMIT 1
